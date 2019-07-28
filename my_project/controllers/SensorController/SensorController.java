e43beecc9e4800c8d2b4ba2bcf744811ce643217
gkhnrsln <goekhan.arslan@outlook.de>
2019-07-28 18:54:33 +0200

    Weitere Sensoren aktiviert
    Kamera, GPS, Gyro, etc..

--- my_project/controllers/SensorController/SensorController.java
+++ my_project/controllers/SensorController/SensorController.java
@@ -10,6 +10,10 @@ public class SensorController {
   static TouchSensor lfoot_lbumper, lfoot_rbumper;  // left foot bumpers
   static TouchSensor rfoot_lbumper, rfoot_rbumper;  // right foot bumpers
   static DistanceSensor[] us = {new DistanceSensor("Sonar/Left"), new DistanceSensor("Sonar/Right")}; // ultrasound sensors
+  static Accelerometer accelerometer;
+  static GPS gps; //gps
+  static Gyro gyro; //gyro
+  static LED led; //led
   
   // motion file handles
   static Motion forWard, gehen, sideStepRight, turnLeft, turnLeft60, turnLeft180, handWave, anfall, up, bauch;
@@ -20,8 +24,19 @@ public class SensorController {
     // camera
     cameraTop = new Camera("CameraTop");
     cameraBottom = new Camera("CameraBottom");
+    cameraTop.enable(4*timeStep);
+    cameraBottom.enable(4*timeStep);
+    //TouchSensor
+    //DistanceSensor
+    //Accelerometer
+    accelerometer = new Accelerometer("accelerometer");
+    accelerometer.enable(timeStep);
+    //GPS
+    gps = new GPS("gps");
+    gps.enable(timeStep);
     // gyro
-    
+    gyro = new Gyro("gyro");
+    gyro.enable(timeStep);
     // ultrasound sensors
     //us[0] = new DistanceSensor("Sonar/Left");
     //us[1] = new DistanceSensor("Sonar/Right");
@@ -97,10 +112,11 @@ public class SensorController {
       startMotion(forWard);
     }
   }
-  
+
   static void runCommand(int key) {
     switch(key) {
-      case 'U': printUltrasoundSensors(); break;
+      case 'a': printUltrasoundSensors(); break;
+      case 3: printUltrasoundSensors(); break;
     }
   }
   
@@ -130,10 +146,9 @@ public class SensorController {
   
     while (robot.step(timeStep) != -1) {
       if(key >= 0) runCommand(key);
-      gefallen();
-      printUltrasoundSensors();
+      //gefallen();
+      //printUltrasoundSensors();
       move();
-      
       key = keyboard.getKey();
     };
   }
