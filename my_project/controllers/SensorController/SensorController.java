1417598d377586420e77fc1a3783a96f1362fbf6
gkhnrsln <goekhan.arslan@outlook.de>
2019-08-31 23:17:04 +0200

    Anpassung der Pfade für die Motiondateien

--- my_project/controllers/SensorController/SensorController.java
+++ my_project/controllers/SensorController/SensorController.java
@@ -1,4 +1,5 @@
 import com.cyberbotics.webots.controller.*;
+import java.io.File;
 
 public class SensorController {
   static Robot robot = new Robot();
@@ -21,27 +22,33 @@ public class SensorController {
   
   // motion file handles
   static Motion forWard, gehen, sideStepRight, turnLeft, turnLeft60, turnLeft180, handWave, anfall, up, bauch;
-  static String pfad = System.getProperty("user.dir") + System.getProperty("file.separator") + "Motions"
-      + System.getProperty("file.separator");
-      
+  static String fs = System.getProperty("file.separator");
+  static File file = new File(System.getProperty("user.dir"));   
+  static String pfad;
+  
   static void findAndEnableDevices() {
     // inertialUnit
     inertialUnit = new InertialUnit("InertialUnit");
     inertialUnit.enable(timeStep);
+    
     // camera
     cameraTop = new Camera("CameraTop");
     cameraBottom = new Camera("CameraBottom");
     cameraTop.enable(4*timeStep);
     cameraBottom.enable(4*timeStep);
+    
     //Accelerometer
     accelerometer = new Accelerometer("accelerometer");
     accelerometer.enable(timeStep);
+    
     //GPS
     gps = new GPS("gps");
     gps.enable(timeStep);
+    
     // gyro
     gyro = new Gyro("gyro");
     gyro.enable(timeStep);
+    
     // ultrasound sensors
     //us[0] = new DistanceSensor("Sonar/Left");
     //us[1] = new DistanceSensor("Sonar/Right");
@@ -59,6 +66,7 @@ public class SensorController {
     lfoot_rbumper.enable(timeStep);
     rfoot_lbumper.enable(timeStep);
     rfoot_rbumper.enable(timeStep);
+    
     //keyboard
     keyboard.enable(10 * timeStep);
   }
@@ -71,6 +79,9 @@ public class SensorController {
   
   // load motion files
   public static void loadMotionFiles() {
+    file = (file.getParentFile().getParentFile());
+    pfad = file.getPath() + fs + "motions" + fs;
+    
     handWave = new Motion(pfad + "HandWave.motion");
     forWard = new Motion(pfad + "Gehen50.motion");
     gehen = new Motion(pfad + "Gehen50Anfang.motion");
