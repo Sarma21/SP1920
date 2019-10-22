7db7f17474a833c053417c81ec92031f1dcc1df4
gkhnrsln <53009328+gkhnrsln@users.noreply.github.com>
2019-10-22 13:06:06 +0200

    Footsensor hinzugefügt

--- my_project/controllers/StandUpTest/StandUpTest.java
+++ my_project/controllers/StandUpTest/StandUpTest.java
@@ -9,6 +9,7 @@ public class StandUpTest {
   
   // simulated devices
   static Accelerometer accelerometer;
+  static TouchSensor[] fsr = {new TouchSensor("LFsr"), new TouchSensor("RFsr")};                        // force sensitive resistors
   
   // motion file handles
   static Motion handWave, standUpFromFront, forwards50;
@@ -17,6 +18,10 @@ public class StandUpTest {
     //Accelerometer
     accelerometer = new Accelerometer("accelerometer");
     accelerometer.enable(timeStep);
+    
+    // foot sensors
+    fsr[0].enable(timeStep);
+    fsr[1].enable(timeStep);
   }
   
   // load motion files
@@ -38,22 +43,71 @@ public class StandUpTest {
     } while (!currentlyPlaying.isOver());
     currentlyPlaying = null;
   }
-
-  static void move() {
-    //
+static double clamp(double value, double min, double max) {
+  if (min > max) {
+    //assert(0);
+    return value;
   }
+  return value < min ? min : value > max ? max : value;
+}
+
+static void print_foot_sensors() {
+// force sensor values
+  double[][] fsv = {
+    fsr[0].getValues(),
+    fsr[1].getValues()
+  };
+  
+  double newtonLeft = 0, newtonRight = 0;
+
+  // The coefficients were calibrated against the real
+  // robot so as to obtain realistic sensor values.
+  double l[] = {
+    fsv[0][2] / 3.4 + 1.5 * fsv[0][0] + 1.15 * fsv[0][1],  // Left Foot Front Left
+    fsv[0][2] / 3.4 + 1.5 * fsv[0][0] - 1.15 * fsv[0][1],  // Left Foot Front Right
+    fsv[0][2] / 3.4 - 1.5 * fsv[0][0] - 1.15 * fsv[0][1],  // Left Foot Rear Right
+    fsv[0][2] / 3.4 - 1.5 * fsv[0][0] + 1.15 * fsv[0][1],  // Left Foot Rear Left
+  };
+  double r[] = {
+    fsv[1][2] / 3.4 + 1.5 * fsv[1][0] + 1.15 * fsv[1][1],  // Right Foot Front Left
+    fsv[1][2] / 3.4 + 1.5 * fsv[1][0] - 1.15 * fsv[1][1],  // Right Foot Front Right
+    fsv[1][2] / 3.4 - 1.5 * fsv[1][0] - 1.15 * fsv[1][1],  // Right Foot Rear Right
+    fsv[1][2] / 3.4 - 1.5 * fsv[1][0] + 1.15 * fsv[1][1],  // Right Foot Rear Left
+  };
+
+  int i;
+  for (i = 0; i < 4; ++i) {
+    l[i] = clamp(l[i], 0, 25);
+    r[i] = clamp(r[i], 0, 25);
+    newtonLeft += l[i];
+    newtonRight += r[i];
+  }
+
+  System.out.println("----------foot sensors----------\n");
+  System.out.println("   left       right\n");
+  System.out.println("+--------+ +--------+\n");
+  System.out.println("|" + (float) l[0] +" "+ (float) l[1] + "| |" + (float) r[0] +" "+ (float) r[1] + "| front\n");
+  System.out.println("|        | |        |\n");
+  System.out.println("|" + (float) l[3] +" "+ (float) l[2] + "| |" + (float) r[3] +" "+ (float) r[2] + "| back\n");
+  System.out.println("+--------+ +--------+\n");
+  System.out.println("total: " + (float) (newtonLeft + newtonRight) + " Newtons, " + (float) ((newtonLeft + newtonRight) / 9.81) + " kilograms\n");
+}
+
  
   public static void main(String[] args) {
     // initialize stuff
     findAndEnableDevices();
     loadMotionFiles();
-    //startMotion(forwards50);
-
+    //startMotion(handWave);
+    
     while (robot.step(timeStep) != -1) {
       acc = accelerometer.getValues();
+      print_foot_sensors();
       //Bei einem "Sturz" von von 0.05 m steht der Roboter erst auf.
+      //if((currentlyPlaying == null || currentlyPlaying.isOver()) && Math.abs(acc[0]) > Math.abs(acc[1]) && Math.abs(acc[0]) > Math.abs(acc[2]) && Math.abs(acc[0]) < -1){
       if((currentlyPlaying == null || currentlyPlaying.isOver()) && acc[0] > acc[1] && acc[0] > acc[2] && acc[0] < -5){
         System.out.println("currentlyPlaying == null");
+        
         startMotion(standUpFromFront);
       } else {
         //laufe weiter
