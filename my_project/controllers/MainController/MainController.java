877b0a8fee89c60009ed769f24f8d2b93d0e467f
hannes90 <hhergerdt@yahoo.de>
2019-12-24 16:12:51 +0100

    MainController
    Falschen Controller Hochgeladen. Das ist der richtige

--- my_project/controllers/MainController/MainController.java
+++ my_project/controllers/MainController/MainController.java
@@ -26,14 +26,22 @@ public class MainController {
   static int rr2 = 0;
   static int gr2 = 0;
   static int br2 = 0;
-  //TopCamera RGB eines bestimmten Bereichs
+  //TopCamera RGB eines bestimmten Bereiches
+  //Alle werte des Array = 0; ---> loadMotionsFiles()
+  static int[][] rgbTop = new int[100][3];
+  static int[][] rgbBottom = new int[100][3];
+  //Sackgassen zähler
+  static int sackGasse = 0;
+  
   static int r1 = 0;
   static int g1 = 0;
   static int b1 = 0; 
-  //BottomCamera RGB eines bestimmten Bereichs
+  //BottomCamera RGB eines bestimmten Bereiches
   static int r2 = 0;
   static int g2 = 0;
   static int b2 = 0;
+  //Schrittzähler
+  static int stepCounter = 0; 
   
   /*------Später werden sie in ein Array gepackt!-------*/
     
@@ -48,72 +56,63 @@ public class MainController {
   static LED led; //led
   
   // motion file handles
-  static Motion forwards, forwards50, gehen, turnLeft40, turnLeft60, turnRight40, turnRight60, handWave, standUpFromFront, bauch;
+  static Motion forwards, turnLeft40, turnLeft60, turnRight40, turnRight60, turn180, turnHeadLeft, turnHeadRight, turnHeadForward, standToSit, sitToStand, handWave, standUpFromFront;
     
    static void startMotion(Motion motion){
-    motion.play();
-    do {
-    robot.step(timeStep);
-    } while(! motion.isOver());
+      motion.play();
+      do {
+      robot.step(timeStep);
+      } while(!motion.isOver());
    }
   
 
- static void findAndEnableDevices() {
-  //Accelerometer
-  accelerometer = new Accelerometer("accelerometer");
-  accelerometer.enable(timeStep);
-  // camera
-  cameraTop = new Camera("CameraTop");
-  cameraBottom = new Camera("CameraBottom");
-  cameraTop.enable(4*timeStep);
-  cameraBottom.enable(4*timeStep);
-  // ultrasound sensors
-  sonar[0].enable(timeStep);
-  sonar[1].enable(timeStep);
-
-  /*
-  // inertialUnit
-  inertialUnit = new InertialUnit("InertialUnit");
-  inertialUnit.enable(timeStep);
-  
-  //GPS
-  gps = new GPS("gps");
-  gps.enable(timeStep);
-  
-  // gyro
-  gyro = new Gyro("gyro");
-  gyro.enable(timeStep);
-  
-  //keyboard
-  keyboard.enable(10 * timeStep);
-  
-  //foot bumpers
-  lfoot_lbumper = new TouchSensor("LFoot/Bumper/Left");
-  lfoot_rbumper = new TouchSensor("LFoot/Bumper/Right");
-  rfoot_lbumper = new TouchSensor("RFoot/Bumper/Left");
-  rfoot_rbumper = new TouchSensor("RFoot/Bumper/Right");
-  lfoot_lbumper.enable(timeStep);
-  lfoot_rbumper.enable(timeStep);
-  rfoot_lbumper.enable(timeStep);
-  rfoot_rbumper.enable(timeStep);
-  */
-}
+   static void findAndEnableDevices() {
+    //Accelerometer
+    accelerometer = new Accelerometer("accelerometer");
+    accelerometer.enable(timeStep);
+    // camera
+    cameraTop = new Camera("CameraTop");
+    cameraBottom = new Camera("CameraBottom");
+    cameraTop.enable(4*timeStep);
+    cameraBottom.enable(4*timeStep);
+    //RGB Array füllen
+    for (int i = 0; i < 100; i++){
+      for (int j = 0; j < 3; j++){
+        rgbTop[i][j] = 0;
+        rgbBottom[i][j] = 0;
+      }
+    }
+    // ultrasound sensors
+    sonar[0].enable(timeStep);
+    sonar[1].enable(timeStep);
+   }
   
   static void loadMotionsFiles(){
     File file = new File(System.getProperty("user.dir"));
     file = (file.getParentFile().getParentFile());
     String pfad = file.getPath() + System.getProperty("file.separator") + "motions" + System.getProperty("file.separator");
     
+
     forwards = new Motion(pfad + "Forwards.motion");
-    gehen = new Motion(pfad + "Gehen50.motion");
+    //gehen = new Motion(pfad + "Gehen50.motion");
     handWave = new Motion(pfad + "HandWave.motion");
-    turnLeft40 = new Motion(pfad + "TurnLeft40");
-    turnRight40 = new Motion(pfad + "TurnRight40");
+    turnLeft40 = new Motion(pfad + "TurnLeft40.motion");
+    turnRight40 = new Motion(pfad + "TurnRight40.motion");
+    turn180 = new Motion(pfad + "TurnLeft180.motion");
+    turnLeft60 = new Motion(pfad + "TurnLeft60.motion");
+    turnRight60 = new Motion(pfad + "TurnRight60.motion");
+    standToSit = new Motion(pfad + "StandToSit.motion");
+    sitToStand = new Motion(pfad + "SitToStand.motion");
+    //turnHeadLeft = new Motion(pfad + "TurnHeadLeft.motion");
+    //turnHeadRight = new Motion(pfad + "TurnHeadRight.motion");
+    //turnHeadForward = new Motion(pfad + "TurnHeadForward.motion");
+    //turnLeft10 = new Motion(pfad + "TurnLeft10.motion");
+    //turnRight10 = new Motion(pfad + "TurnRight10.motion");
   }
   
   // x gibt an wie viele Pixel von der linken seite du betrachten willst
   // x = 160 für Fullscreen
-  static void leftPixelTop(int x){
+  static void leftRGBTop(int x){
     int[] image = cameraTop.getImage();
     int l1 = 0;
     int l2 = 1;
@@ -143,7 +142,7 @@ public class MainController {
   }
   
     // x gibt an wie viele Pixel von der rechte seite du betrachten willst
-  static void rightPixelTop(int x){
+  static void rightRGBTop(int x){
     int[] image = cameraTop.getImage();
     int l1 = 159-x;
     int l2 = 1;
@@ -174,7 +173,7 @@ public class MainController {
   
   // x gibt an wie viele Pixel du betrachten willst
   // x = 160 für Fullscreen
-  static void leftPixelBottom(int x){
+  static void leftRGBBottom(int x){
     int[] image = cameraBottom.getImage();
     int l1 = 0;
     int l2 = 1;
@@ -202,7 +201,7 @@ public class MainController {
     //System.out.println("red=" + rl2 + "\tgreen=" + gl2 + "\tblue="+ bl2);
   }
   
-    static void rightPixelBottom(int x){
+    static void rightRGBBottom(int x){
     int[] image = cameraBottom.getImage();
     int l1 = 159-x;
     int l2 = 1;
@@ -231,11 +230,12 @@ public class MainController {
     //System.out.println("red=" + rr2 + "\tgreen=" + gr2 + "\tblue="+ br2);
   }
     /*
-      Mir dieser Funktion lässt sich der durchschnitts Farbwert eines bestimmten Pixelbereich errechnen 
+      Mit dieser Funktion lässt sich der durchschnittliche Farbwert eines bestimmten Pixelbereich errechnen 
       x1 gibt an, wie viele Pixel auf der linken Seite übersprungen werden sollen.
       x2 gibt an, wie viele Pixel auf der rechten Seite übersprungen werden sollen.
       y1 gibt an, wie viele Pixel auf der oberen Seite übersprungen werden sollen.
       y2 gibt an, wie viele Pixel auf der unteren Seite übersprungen werden sollen.
+      a gibt an, an welcher stelle die Werte im Array gespeichert werden.
       
       y1 y1 y1 y1 y1 y1 y1 
       y1 y1 y1 y1 y1 y1 y1 
@@ -243,74 +243,118 @@ public class MainController {
       x1 x1 P  P  x2 x2 x2
       y2 y2 y2 y2 y2 y2 y2 
     */
-    static void getPixelTop(int x1, int x2, int y1, int y2){
+    static void getColorsTop(int x1, int x2, int y1, int y2, int a){
       int[] image = cameraTop.getImage();
       int p1 = y1*160+x1;
       int p2 = y1*160+160-x2; //y1*160+x1+160-x1-x2;
       int p3 = y1*160;
-      int p4 = image.length - y2*160;
+      int p4 = image.length-y2*160;
       int x = (120-y1-y2)*160-(x1*(120-y1-y2))-(x2*(120-y1-y2));
-      int zahl = 1;
-      int loop = (y1+zahl)*160;
+      int loop = 1;
+      int loopEnd = (y1+loop)*160;
       for (int i=0; i < image.length; i++){
         int pixel = image[i];
         if (i >= p3 && i <= p4){
           if (i >= p1 && i < p2){
-            r1 += Camera.pixelGetRed(pixel);
-            g1 += Camera.pixelGetGreen(pixel);
-            b1 += Camera.pixelGetBlue(pixel);
-            loop = (y1+zahl)*160;
-            if(i > loop){  
+            rgbTop[a][0] += Camera.pixelGetRed(pixel);
+            rgbTop[a][1] += Camera.pixelGetGreen(pixel);
+            rgbTop[a][2] += Camera.pixelGetBlue(pixel);
+            loopEnd = (y1+loop)*160;
+            if(i > loopEnd){  
               p1 += 160;
               p2 += 160;
-              zahl++;  
+              loop++;  
             }
           }
-
         }
       }
       // image.length/x;
-      r1 /= x;
-      g1 /= x;
-      b1 /= x;
+      rgbTop[a][0] /= x;
+      rgbTop[a][1] /= x;
+      rgbTop[a][2] /= x;
       
-      System.out.println("TopCamera Pixel Farbwerte mit x1: " + x1 + "  x2: " + x2 + "  y1:" + y1 + "  y2:" + y2);
-      System.out.println("red=" + r1 + "    green=" + g1 + "    blue="+ b1);
+      System.out.println(a + ": TopCamera Pixel Farbwerte mit x1: " + x1 + "  x2: " + x2 + "  y1:" + y1 + "  y2:" + y2);
+      System.out.println("red=" + rgbTop[a][0] + "    green=" + rgbTop[a][1] + "    blue="+ rgbTop[a][2]);
     } 
     //Selbe Methode für die BottomCamera
-    static void getPixelBottom(int x1, int x2, int y1, int y2){
+    static void getColorsBottom(int x1, int x2, int y1, int y2, int a){
       int[] image = cameraBottom.getImage();
       int p1 = y1*160+x1;
       int p2 = y1*160+160-x2; //y1*160+x1+160-x1-x2;
       int p3 = y1*160;
-      int p4 = image.length - y2*160;
+      int p4 = image.length-y2*160;
       int x = (120-y1-y2)*160-(x1*(120-y1-y2))-(x2*(120-y1-y2));
-      int zahl = 1;
-      int loop = (y1+zahl)*160;
+      int loop = 1;
+      int loopEnd = (y1+loop)*160;
       for (int i=0; i < image.length; i++){
         int pixel = image[i];
         if (i >= p3 && i <= p4){
           if (i >= p1 && i < p2){
-            r2 += Camera.pixelGetRed(pixel);
-            g2 += Camera.pixelGetGreen(pixel);
-            b2 += Camera.pixelGetBlue(pixel);
-            loop = (y1+zahl)*160;
-            if(i > loop){  
+            rgbBottom[a][0] += Camera.pixelGetRed(pixel);
+            rgbBottom[a][1] += Camera.pixelGetGreen(pixel);
+            rgbBottom[a][2] += Camera.pixelGetBlue(pixel);
+            loopEnd = (y1+loop)*160;
+            if(i > loopEnd){  
               p1 += 160;
               p2 += 160;
-              zahl++;  
+              loop++;  
             }
           }
-
         }
       }
       // image.length/x;
-      r2 /= x;
-      g2 /= x;
-      b2 /= x;
+      rgbBottom[a][0] /= x;
+      rgbBottom[a][1] /= x;
+      rgbBottom[a][2] /= x;
 
-      System.out.println("BottomCamera Pixel Farbwerte mit x1: " + x1 + "  x2: " + x2 + "  y1:" + y1 + "  y2:" + y2);
-      System.out.println("red=" + r2 + "    green=" + g2 + "    blue="+ b2);
+      System.out.println(a + ": BottomCamera Pixel Farbwerte mit x1: " + x1 + "  x2: " + x2 + "  y1:" + y1 + "  y2:" + y2);
+      System.out.println("red=" + rgbBottom[a][0] + "    green=" + rgbBottom[a][1] + "    blue="+ rgbBottom[a][2]);
+    }
+    
+    //Befüllt die RGB Arrays
+    //Das Bild der Camera wird in 100 kleine Bilder zerteilt und im rgbTop und rgbBottom Array gespeichert
+    static void scan() {
+         int x1 = 0;
+         int x2 = 144;
+         int y1 = 0;
+         int y2 = 108;
+         int z = 0;
+         
+         for (int i = 0; i < 10; i++){
+           for (int j = 0; j < 10; j++){
+             getColorsTop(x1, x2, y1, y2, z);
+             getColorsBottom(x1, x2, y1, y2, z);
+             z++;
+             x1 += 16;
+             x2 -= 16;
+           }
+           x1 = 0;
+           x2 = 144;
+           y1 += 12;
+           y2 -= 12;
+         }
+         /*
+         getRGBTop(0, 150, 0, 110, 0); 
+         getRGBTop(150, 0, 0, 110, 1);
+         getRGBTop(0, 150, 110, 0, 2); 
+         getRGBTop(150, 0, 110, 0, 3);
+         getRGBTop(0, 150, 0, 110, 4); 
+         getRGBTop(150, 0, 0, 110, 5);
+         getRGBTop(0, 150, 110, 0, 6); 
+         getRGBTop(150, 0, 110, 0, 7);
+         getRGBTop(0, 150, 0, 110, 8); 
+         getRGBTop(150, 0, 0, 110, 9);
+         getRGBBottom(0, 150, 0, 110, 0); 
+         getRGBBottom(150, 0, 0, 110, 1);          
+         getRGBBottom(0, 150, 110, 0, 2); 
+         getRGBBottom(150, 0, 110, 0, 3);
+         getRGBBottom(0, 150, 0, 110, 4); 
+         getRGBBottom(150, 0, 0, 110, 5);          
+         getRGBBottom(0, 150, 110, 0, 6); 
+         getRGBBottom(150, 0, 110, 0, 7);
+         getRGBBottom(0, 150, 0, 110, 8); 
+         getRGBBottom(150, 0, 0, 110, 9);   
+         */
     } 
   
   static boolean sonarCheck(){
@@ -330,8 +374,8 @@ public class MainController {
   
   static void leftCheck(){
   
-    leftPixelBottom(10);
-    rightPixelBottom(10);
+    leftRGBBottom(10);
+    rightRGBBottom(10);
   
     if(100 < rl2 && rl2 < 120 && 121 < gl2 && gl2 < 241 && 137 < bl2 && bl2 < 157){
          startMotion(turnRight40);
@@ -341,73 +385,74 @@ public class MainController {
 
   static void rightCheck(){
   
-    leftPixelBottom(10);
-    rightPixelBottom(10);
+    leftRGBBottom(10);
+    rightRGBBottom(10);
        
     if(100 < rr2 && rr2 < 120 && 121 < gr2 && gr2 < 241 && 137 < br2 && br2 < 157){
         startMotion(turnLeft40);
     }    
-       /*
-       int x = 0;
-      
-      if (111 < rl1 && rl1 < 131 && 109 < gl1 && gl1 < 129 && 104 < bl1 && bl1 < 124){
-          if (197 < rr1 && rr1 < 217 && 144 < gr1 && gr1 < 164 && 98 < bl1 && bl1 < 118) {
-              x = x + 1;
-          }
-          x = x + 1;
-      }
-      
-      if (114 < rl2 && rl2 < 134 && 119 < gl2 && gl2 < 139 && 121 < bl2 && bl2 < 141){
-          if (89 < rr2 && rr2 < 109 && 106 < gr2 && gr2 < 126 && 121 < bl2 && bl2 < 141) {
-              x = x + 1;
-          }
-          x = x + 1;
-      }
-      
-      System.out.println("X: " + x);
-      
-      if (x > 3){
-          return true;
-      }   
-      */
   }
   
   static void move(){
-  
-     if (!finish) {
-       //prüft ob ziel erreicht wurde
-       if (false){
+     boolean leftPathAvailable = false;
+     boolean rightPathAvailable = false;
+
+     scan();
+
+     if (false){   // 
          finish = true;
+     }
+     //prüft ob ziel erreicht wurde
+     if (!finish) {
+        //alle 4 schritte werden die seiten geprüft
+        if (stepCounter == 4){
+          startMotion(turnLeft40); //turnHeadLeft ergänzen #73
+          scan();
+          startMotion(turnRight40); //turnHeadForward ergänzen #73
+          if (5 < rgbTop[34][0] && rgbTop[34][0] < 15 && 4 < rgbTop[34][1] && rgbTop[34][1] < 13 && 1 < rgbTop[34][2] && rgbTop[34][2] < 11){
+            if (5 < rgbTop[35][0] && rgbTop[35][0] < 15 && 4 < rgbTop[35][1] && rgbTop[35][1] < 13 && 1 < rgbTop[35][2] && rgbTop[35][2] < 11){
+              leftPathAvailable = true;
+            }
+          } 
+          stepCounter = 0;
        }
-       if (sonarCheck()){
-        //TurnHead
-       }     
-       //Weg korrektur nötig?
-       /*
-       40 Pixel prüfung
-       Top     Links r102 g122 137  Rechts r114 g123 b134
-       Bottom  Links r124 g129 b131 Rechts r206 g156 b109
-       80 Pixel prüfung
-       Top     Links r105 g120 b131 Rechts r120 g124 b129
-       Bottom  Links r161 g139 b118 Rechts r205 g154 b107 
        
-       40 Pixel prüfung
-       Top     Links r121 g119 b114 Rechts r99 g116 b131
-       Bottom  Links r207 g154 b108 Rechts r99 g116 b131
-       80 Pixel prüfung
-       Top     Links r116 g118 b118 Rechts r99 g116 b130
-       Bottom  Links r200 g151 b108 Rechts r114 g121 b127
-       */
-       //leftCheck();
-        //10° nach rechts dreh
-      // rightCheck();
-        //10° nach links drehen 
-        
-        getPixelTop(2, 156, 1, 118);
-        getPixelBottom(2, 156, 1, 118);
-       startMotion(forwards);
-   }
-    
+       if (sonarCheck()){
+          startMotion(turnLeft40);  //turnHeadLeft ergänzen #73
+          scan();
+          startMotion(turnRight40);  //turnHeadForward ergänzen #73
+          if (5 < rgbTop[34][0] && rgbTop[34][0] < 15 && 4 < rgbTop[34][1] && rgbTop[34][1] < 13 && 1 < rgbTop[34][2] && rgbTop[34][2] < 11){
+            if (5 < rgbTop[35][0] && rgbTop[35][0] < 15 && 4 < rgbTop[35][1] && rgbTop[35][1] < 13 && 1 < rgbTop[35][2] && rgbTop[35][2] < 11){
+              leftPathAvailable = true;
+            }
+          } else {
+              startMotion(turnLeft40);  //turnHeadLeft ergänzen #73
+              scan();
+              startMotion(turnRight40);  //turnHeadForward ergänzen #73
+              if (5 < rgbTop[34][0] && rgbTop[34][0] < 15 && 4 < rgbTop[34][1] && rgbTop[34][1] < 13 && 1 < rgbTop[34][2] && rgbTop[34][2] < 11){
+                if (5 < rgbTop[35][0] && rgbTop[35][0] < 15 && 4 < rgbTop[35][1] && rgbTop[35][1] < 13 && 1 < rgbTop[35][2] && rgbTop[35][2] < 11){
+                  rightPathAvailable = true;
+                }
+            }
+           }
+           if (leftPathAvailable == true) {
+               startMotion(turnLeft40);
+               leftPathAvailable = false;
+           } else if (rightPathAvailable == true) {
+               startMotion(turnRight40);
+               rightPathAvailable = false;
+           } else {
+
+          }
+         startMotion(forwards); 
+         stepCounter++;
+       } 
+     
+     //leftCheck();
+     //10° nach rechts dreh
+     //rightCheck();
+     //10° nach links drehen         
+ 
   }
 
   public static void main(String[] args) {
